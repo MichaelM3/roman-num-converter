6da60c4096bd300162bb49b11d0cd3af5837d352
michaelm3 <michael.muniz94@gmail.com>
2022-05-26 11:47:06 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const romanNums = { i: 1, v: 5, x: 10, l: 50, c: 100, d: 500, m: 1000, V: 5000, X: 10000, L: 50000, C: 100000, D: 500000, M: 1000000 }
+
+
+  return (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App
